a2a339535080441d048345643696f45dc5a61bc1
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-04-11 21:20:08 -0400

    Release: 1.0.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ LONG_DESCRIPTION = \
 
 setup(
     name="ultyas",
-    version="1.0.4",
+    version="1.0.5",
     packages=find_packages(),
     description=("A tool for converting code snippets from "
                  "Ultisnips to YASnippet format"),
